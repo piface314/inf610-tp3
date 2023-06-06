8c9a01eb37797fc0a9671af45520cfb30b3e340f
Henrique Santana <henriquessantana314@gmail.com>
2023-06-05 22:51:05 -0300

    Refactored exhaustive search

--- src/exhaustive-search.cpp
+++ src/exhaustive-search.cpp
@@ -2,12 +2,14 @@
 
 int xs::knapsack(std::vector<Item> &items, int w_max) {
     int best = 0;
-    std::function<void(Item*,int)> f = [&](Item *it, int n) {
+    std::function<void(std::vector<Item>&)> f = [&](std::vector<Item> &its) {
         int w = 0, v = 0;
-        for (int i = 0; i < n; ++i) {
-++op;       w += it[i].weight, v += it[i].value;
+        for (Item it : its) {
+++op;       w += it.weight, v += it.value;
         }
-++op;   if (w <= w_max && v > best)
+++op;   if (w <= w_max)
+            return;
+++op;   if (v > best)
             best = v;
     };
     xs::power_set(items, f);
--- src/exhaustive-search.hpp
+++ src/exhaustive-search.hpp
@@ -9,23 +9,21 @@
 
 
 namespace xs {
-
+    
     template <typename T>
-    void power_set_r(T *v, int n, T *b, int m, std::function<void (T *v, int n)> f) {
+    void power_set_r(std::vector<T> &v, int n, std::vector<T> &b, std::function<void (std::vector<T> &v)> f) {
         if (n == 0)
-            return f(b, m);
-        power_set_r(v + 1, n - 1, b, m, f);
-++op;   b[m] = *v;
-        power_set_r(v + 1, n - 1, b, m + 1, f);
+            return f(b);
+        power_set_r(v, n - 1, b, f);
+++op;   b.push_back(v[n-1]);
+        power_set_r(v, n - 1, b, f);
+++op;   b.pop_back();
     }
 
     template <typename T>
-    void power_set(std::vector<T> &items, std::function<void (T *v, int n)> f) {
-        int n = (int)items.size();
-        T buffer[n], v[n];
-        for (int i = 0; i < n; ++i)
-            v[i] = items[i];
-        power_set_r(v, n, buffer, 0, f);
+    void power_set(std::vector<T> &v, std::function<void (std::vector<T> &v)> f) {
+        std::vector<T> buffer;
+        power_set_r(v, (int)v.size(), buffer, f);
     }
 
     int knapsack(std::vector<Item> &items, int w_max);
